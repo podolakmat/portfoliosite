eacdd71b0d4dbc5fc09a0dfd3c4c7a288c299944
podolakmateusz <39656669+podolakmateusz@users.noreply.github.com>
2018-12-12 20:29:22 +0100

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,38 +1,15 @@
-/*menu hamburger */
-const menuBtn = document.querySelector('.hamburger-icon');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.nav');
-const navItems = document.querySelectorAll('.list-element');
-
-let showMenu = false;
-
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    if (!showMenu) {
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
-
-        showMenu = true;
-    } else {
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
-
-        showMenu = false;
-    }
-}
-/*add active link class*/
-document.addEventListener('click', function() {
-    if (!event.target.classList.contains('link')) return;
-    event.target.classList.add('current');
-
-    var links = document.querySelectorAll('.link');
-    for (var i = 0; i < links.length; i++) {
-        if (links[i] === event.target) continue;
-        links[i].classList.remove('current');
-    }
-}, false);
+var navigation = responsiveNav(".nav-collapse", {
+    animate: true,
+    transition: 284,
+    label: "Menu",
+    insert: "before",
+    customToggle: "",
+    closeOnNavClick: true,
+    openPos: "relative",
+    navClass: "nav-collapse",
+    navActiveClass: "js-nav-active",
+    jsClass: "js",
+    init: function() {},
+    open: function() {},
+    close: function() {}
+});
